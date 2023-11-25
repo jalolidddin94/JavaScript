4859bdd1bcc64effe3fa2bbf564e6748402af0b9
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-11-26 02:14:47 +0900

    if else homework

---
+++ lesson4/script.js
@@ -0,0 +1,17 @@
+// let num = 6;
+// num % 3 === 0 && console.log('3 ga bulinadi');
+
+// let num1 = 10;
+// num % 5 === 0 && console.log('5 ga bulinadi');
+
+// let num2 = 30;
+// num % 3 === 0 && console.log('3 ga va 5 ga bulinadi');
+
+// if(browser == 'Edge') { alert("You've got the Edge!"); } 
+// else if (browser == 'Chrome' ||  browser == 'Firefox' ||  browser == 'Safari' || browser == 'Opera') { alert( 'Okay we support these browsers too' ); } 
+// else { alert( 'We hope that this page looks ok!' ); }
+
+// let message = (login == 'Employee') ? 'Hello' :
+//   (login == 'Director') ? 'Greetings' :
+//   (login == '') ? 'No login' :
+//   '' ;
