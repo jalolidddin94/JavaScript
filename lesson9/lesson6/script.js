541b05ee4d2dc9f8e1eb2c43c709f5a521525468
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-11-26 02:03:16 +0900

    for loop

---
+++ lesson9/lesson6/script.js
@@ -0,0 +1,31 @@
+// let str = 'jaloliddin';
+
+// for(let i = str.length - 1; i >= 0; i--){
+// console.log(str[i]);
+// }
+
+// for(let i=1; i<=10; i++){
+//     if (i%2 ===0)
+//     console.log(i,'juft son');
+//     else{
+
+//      console.log(i, 'toq son');
+//     }
+// }
+
+// let str = "WebBeainAcadAmya"
+
+// var string = ''
+
+// for(let i=0; i<str.length; i++){
+//     if(str[i] === 'a'  || str[i] === 'A'){
+//         string += str[i]
+//     }
+// }
+
+// console.log('A', 'a-',string.length);
+
+
+
+
+
