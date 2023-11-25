fb2bf842e7df874a94dc76df7fba8ca1689e2c6e
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-11-26 02:34:40 +0900

    variables

---
+++ lesson3/script.js
@@ -0,0 +1,79 @@
+let num = 4;
+num === 4 && console.log('juft son');
+
+let num1 = 5;
+num1 === 5 && console.log('toq son');
+
+
+let meva ='shaftoli';
+meva === 'shaftoli' && console.log('10000W');
+
+let meva1 ='anor';
+meva1 === 'anor' && console.log('15000W');
+
+let meva2 ='gilos';
+meva2 === 'gilos' && console.log('15000W');
+
+let num2 = 4;
+num2 === 4 && console.log('juft son');
+
+let num3 = 5;
+num3 === 5 && console.log('toq son');
+
+
+let meva3 ='shaftoli';
+meva3 === 'shaftoli' && console.log('10000W');
+
+let meva4 ='anor';
+meva4 === 'anor' && console.log('15000W');
+
+let meva5 ='gilos';
+meva5 === 'gilos' && console.log('15000W');
+
+let meva6 ='anor';
+meva6 === 'anor' && console.log('unday meva yuq');
+
+let ball3 = 95;
+ball3 <= 100 && console.log('A');
+ball3 => 86 && console.log('A');
+
+let ball = 80;
+ball = 85 && console.log('B');
+ball => 61 && console.log('B');
+
+let ball1 = 60;
+ball1 = 60 && console.log('C');
+ball1 => 50 && console.log('C');
+
+let ball2 = 65;
+ball2 => 45 && console.log('faild');
+ball2 !=45  && console.log('faild');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
