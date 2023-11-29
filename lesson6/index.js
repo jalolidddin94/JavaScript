362558e6d6667aa981af3fd0d57f18356f15f77b
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-11-30 00:49:44 +0900

    lesson6

---
+++ lesson6/index.js
@@ -0,0 +1,30 @@
+// 1-masala
+// let str ='   jaloliddin   ';
+// console.log(str.trim().toLowerCase().includes('n'));
+// console.log(str.trim().toUpperCase().includes('N'));
+
+// 2-masala
+// let str1 = 'Web'
+// let str2 = 'Brain'
+// let str3 = str1
+// str1 = str2
+// str2 = str3
+// console.log(str1);
+
+// 3-masala
+// let yil = 2020;
+// yil % 4 === 0 && console.log('Kabisa yili');
+// yil % 4 !== 0 && console.log('Kabisa yili emas');
+
+// // 4-masala
+// let str = ' WebBrain ' 
+// // console.log(str.repeat(10));
+// console.log(str.repeat(10).replace(/Web/g, 'IT'));
+
+
+
+
+
+
+
+
