54bf11f58d55598d05a0d7201eacac270f52e1cc
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-12-01 01:50:08 +0900

    number.function

---
+++ lesson9/index.js
@@ -0,0 +1,36 @@
+// // 1-masala
+// function jaloliddinpow(son, daraja) {
+//   return son ** daraja;
+// }
+// console.log(jaloliddinpow(2, 5));
+
+// // 2-masala
+// function jaloliddinsqrt(son, daraja) {
+//   return son ** 0.5;
+// }
+// console.log(jaloliddinsqrt(64));
+
+// // 3-masala
+// function jaloliddincbrt(raqam, kub) {
+//   return raqam ** (1 / 3);
+// }
+// console.log(jaloliddincbrt(125).toFixed());
+
+// // 4-masala
+// function homeworkabs(son) {
+//   return (son > 0 && son) || (son < 0 && son * -1);
+// }
+// console.log(homeworkabs(8));
+
+// // 5-masala
+// function butunsonfloor(son) {
+//   return son % 2 === 0 || parseInt(son);
+// }
+// console.log(butunsonfloor(10.7));
+
+// //6-masala
+// function akmalround(son){
+//     son >= parseInt(son) + 0.5 && console.log(parseInt(son) + 1);
+//     son <  parseInt(son)+ 0.5  && console.log(parseInt(son));
+// }
+// akmalround(90.2)
