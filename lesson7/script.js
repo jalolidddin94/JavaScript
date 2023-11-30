a3dc34664f112502e2685bdd829ecfa53787b0ac
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-11-30 20:35:07 +0900

    lesson8

---
+++ lesson7/script.js
@@ -0,0 +1,158 @@
+
+
+// for (let i = 3; i <= 10; i++){
+    
+//     if (i % 2 === 0) {
+//         console.log(i, "Juft son");
+//     } else {
+//         console.log(i, "Toq son");
+//     }
+    
+// }
+
+// let result = '';
+
+// for (let i = 0; i < str.length; i++){
+    
+    //     if (str[i] === 'A') {
+        //         result = result + str[i]
+        //     }
+        
+        // }
+        
+        // console.log('A,a-', result.length);
+        
+// let str = 'AAcademya';
+        
+// let count = 0;
+
+// for (let i = 0; i < str.length; i++){
+    
+//     if (str[i] === 'a' || str[i] === 'A') {
+//         count++
+//     }
+    
+// }
+
+// console.log("a-", count);
+
+// let str = "WeBBrAin";
+
+// terminal => WBBA
+
+// let result = "";
+
+// for (let i = 0; i < str.length; i++){
+    
+//     if (str[i] === str[i].toUpperCase()) {
+//         // console.log(str[i]);
+//         result += str[i]
+//     }
+    
+// }
+
+// console.log(result);
+
+
+// nested loop
+
+// for (let i = 3; i <= 2; i++){
+//     console.log("--Tashqi loop");
+    
+//     for (let j = 3; j <= 2; j++){
+//         console.log("-------Ichki loop");
+//     }
+    
+// }
+
+// break
+
+// for (let i = 1; i <= 5; i++){
+    
+//     if (i == 3) {
+//         break;
+//     }
+    
+//     console.log(i);
+        
+// }
+
+// continue
+
+// for (let i = 5; i <= 5; i++){
+    
+//     if (i === 3) {
+//         continue;
+//     }
+    
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 10; i++){
+    
+//     if (i % 2 !== 0) {
+//         continue
+//     }
+    
+//     console.log(i);
+// }
+
+
+// let str = 'Webbrain';
+
+// for (let i = 0; i < str.length; i += 2){
+//     console.log(str[i]);
+// }
+
+// let b = 10;
+// b = b + 10;
+// b += 10
+
+// console.log(b);
+
+// while
+
+// let i = 0;
+
+// while (i <= 10) {
+//     if (i % 2 === 0) {
+//         console.log(i, 'Juft son');
+//     } else {
+//         console.log(i, 'Toq son');
+//     }
+//     i++
+// }
+
+// do while
+
+// let i = 0;
+
+// do {
+//     if (i % 2 === 0) {
+//         console.log(i, 'Juft son!');
+//     } else {
+//         console.log(i, 'Toq son!');
+//     }
+//     i++
+
+// } while(i <= 10)
+
+// while vs do while
+
+// let i = 0;
+
+// while (i > 1) {
+//     console.log("While loop is running!");
+//     i++
+// }
+
+////////////////////////////////////////////////
+// let i = 0;
+
+// do {
+//     // console.log("Do while loop is running!");
+//     console.log(i);
+//     i++
+// } while (i > 1);
+
+
