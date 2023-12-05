d06965d72d0656c3800f43e3dbb993ac7e5fec35
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-12-05 23:21:55 +0900

    lesson10

---
+++ lesson10/index.js
@@ -0,0 +1,53 @@
+// 1-masala
+// let obj ={
+//     id : 1,
+//     name : "WebBrain"
+// }
+//  for(let  key in obj){
+// console.log((key));
+//  }
+
+// 2-masala
+
+//  let obj = {
+// id : 1,
+// name : "Jaloliddin"
+//  }
+//  console.log("id : " +obj.id);
+
+// 3-masala
+
+// let obj ={
+//     id : 1,
+//     name : "Jaloliddin",
+//     offline : true,
+//     online : true,
+//     individual : false
+// }
+
+// let obj1 = Object.assign({},obj)
+// console.log(obj1);
+
+// 4 - masala
+
+// let person = {
+//   id: 1,
+//   Name: "Jaloliddin",
+//   surname: "Karshiev",
+//   Topik: 5,
+//   student: true,
+//   parol: 101,
+// };
+
+// function example() {
+//   let bazadagiid = person.id;
+//   let bazadaginame = person.Name;
+
+//   if (bazadagiid === person.id && bazadaginame === person.Name) {
+//     console.log("Xush kelipsiz!")
+//   } else {
+//     console.log("Login yoki parol xato")
+//   }
+// }
+
+// example();
---
+++ zoom/zoom.js
@@ -0,0 +1,41 @@
+// function types => decloration, expression and arrow
+
+// function exmple(){
+//     console.log("hello world");
+// }
+// exmple()
+
+// function exmple(){
+//     return "hello world"
+// }
+// console.log( exmple());
+
+// function exmple(){
+//     console.log("G24");
+// }
+// exmple()
+
+// expretion
+
+// var exmple = function(){
+//     console.log("Assalomu alaykum");
+// }
+// exmple();
+
+// var exmple = () =>{
+//     console.log("Vaalaykum assalom");
+// }
+
+// exmple();
+
+var exmple = () => {return "vaalaykum assalom"
+} ;
+
+
+console.log(exmple());
+   
+
+    
+    
+
+
