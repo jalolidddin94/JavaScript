b49ec9bf2c8583cdec588cc6687e20adc88b0818
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-12-11 00:03:03 +0900

    lesson12

---
+++ lesson12/index.js
@@ -0,0 +1,66 @@
+// 1 - masala
+
+let str = 'ABBACC';
+
+console.log(str.split("").sort().splice(2 , 1).join(""));
+
+// 2 - masala
+
+// let cars = [
+//     {id: 1, year: 1998, engine: 1, name: "tico"},
+//     {id: 2, year: 2005, engine: 1.2, name: "matiz"},
+//     {id: 3, year: 2010, engine: 1.6, name: "jentra"},
+//     {id: 4, year: 2010, engine: 1.5, name: "cobolt"},
+//     {id: 5, year: 2012, engine: 2, name: "malibu"},
+//     {id: 6, year: 2000, engine: 1.2, name: "damas"},
+//     {id: 7, year: 2018, engine: 2.4, name: "tracer"},
+// ]
+
+// let car = cars.filter((value) => value.year < 2000);
+// console.log(car);
+
+// let car = cars.filter((value) => value.year > 2010);
+// console.log(car);
+
+// let car = cars.sort((a,b) => b.engine - a.engine );
+// console.log(car);
+
+
+// let car = cars.sort((a,b) => b.year - a.year );
+// console.log(car);
+
+// let car = cars.sort((a,b) => a.name.localeCompare (b.name) );
+// console.log(car);
+
+
+// let filtered = cars.filter(value => value.year < 2000).map((value) =>{
+//     return {...value, status: 'Eski'}
+// });
+
+// console.log(filtered);
+
+
+
+// let filtered = cars.filter(value => value.year >= 2000  &&  value.year <= 2010 ).map((value) =>{
+//     return {...value, status: 'Urta'}
+// });
+
+// console.log(filtered);
+
+
+
+
+// let filtered = cars.filter(value => value.year >= 2010  &&  value.year <= 2022 ).map((value) =>{
+//     return {...value, status: 'Yangi'}
+// });
+
+// console.log(filtered);
+
+
+
+
+
+
+
+
+
