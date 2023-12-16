b973fd9c6924addb7b50396ccff7347b9194dff0
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-12-16 23:40:57 +0900

    Variables, Data Types and If else

---
+++ lesson14/index.js
@@ -0,0 +1,4 @@
+let arr = [4, 10, 888, 99, 100]
+
+let arr1 =arr.filter((value => value > 888));
+console.log(arr1);
---
+++ t.lessons1~3/index.js
@@ -0,0 +1,59 @@
+// let name ="Italiya"
+// console.log(`''Hello'' ${name}`);
+
+// let hello = "Welcome"
+// console.log(`"Jaloliddin" ${hello}`);
+
+// let example = "WebBrin Academy"
+// console.log(`"Assalomu alaykum" ${example}, "IT Clobal Center"`);
+
+// let num = 1,
+// b = 2,
+// c = 3;
+// console.log(num, b, c);
+
+// console.log('Welcome \nto "WebBrain"');
+
+// console.log("assalomu alaykum \n'Jaloliddin");
+
+
+// let b = null;
+//Primitive
+// console.log(b === b);
+
+//nonprimitive
+// console.log({b} === {b});
+
+//////////////////////////////////////////////////
+
+// let a = 1,
+// b = 1;
+// let c = ++a;
+// let d = b ++;
+// console.log(c);
+// console.log(d);
+
+// let a =2;
+// let x = 1 +(a *= 2);
+// console.log(x);
+    
+
+
+// let message;
+
+// if (login == 'Employee') {
+//   message = 'Hello';
+// } else if (login == 'Director') {
+//   message = 'Greetings';
+// } else if (login == '') {
+//   message = 'No login';
+// } else {
+//   message = '';
+// }
+
+// let message = (login =='Employee') ? 'Hello' :
+// (login == 'Director') ? 'Greetings' :
+// (login =='') ? 'No login' : ''
+
+
+
