a4df69770983f50c8e543af8931117ae7511930c
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-12-17 22:08:11 +0900

    object basics and for loop with in

---
+++ t.lessons6~/index.js
@@ -0,0 +1,63 @@
+// OBJECT
+// const account = {
+//     name: "Jaloliddin",
+//     Age : 29,
+//     country : "Uzbekistan",
+//     major: "Frontend"
+// }
+// Object.seal(account);
+//  account.major = "Developer"
+// console.log(account);
+
+// let name = "Jaloliddin"
+// let major = "Frontend"
+// const [ac1] = {
+//     name : name,
+//     major : major
+// }
+// const ac2 ={};
+// ac1.name = "test"
+// Object.assign(ac2, ac1);
+// console.log(ac2);
+
+// let obj = {
+//     id : "12345J@",
+//     password : 12345
+// }
+// for( keys  in obj){
+//     console.log(keys);
+// }
+
+// let obj = {
+//     name : "jaloliddin",
+//     age : 40,
+//     child :{
+//         name : "Fozil",
+//         age : 29,
+//        child : {
+//         name : "Oxunjon",
+//         age : 27,
+//        },
+// },
+// };
+// sum = 0;
+// while(1){
+//     if(obj) sum  +=  obj.age;
+//     else  break;
+//     obj = obj.child;
+//     }
+// console.log(sum);
+
+
+
+//for loop with in
+
+// const ac1 = {
+//     name: "jaloliddin",
+//     student: "WebBrain Academy",
+//     major: "Frontend",
+// }
+// for(let i in ac1){
+//     console.log(i);
+// }
+
