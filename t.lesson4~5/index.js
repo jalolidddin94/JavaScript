73417c7943919d85c89bd16327f2b62cc45532f3
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-12-18 19:28:13 +0900

    Do/Do while, Function

---
+++ t.lesson4~5/index.js
@@ -0,0 +1,73 @@
+//WHILE / DO WHILE
+// i = 3;
+// while (i){
+//     console.log(i);
+//     i--;
+// }
+
+// i = 10;
+
+// while(i){
+//     console.log(i);
+//     i -= 2;
+// }
+
+// do{
+//     console.log(i);
+//     i -= 2;
+// }while(i);
+
+// i = 20;
+// while(i){
+//     console.log(i);
+//  i -=2
+// };
+
+// do{
+//     console.log(i);
+//     i -=2
+// }while(i);
+
+
+//FUNCTION DECLORATION
+// function example(){
+//     console.log("hello word");
+//     console.log("hello word");
+//     console.log("hello word");
+//     console.log("hello word");
+//     console.log("==================");
+// }
+// example()
+// example()
+
+
+// FUNCTION EXPRESSION
+// const example = function(name,surname,age){
+//     console.log(`Name: ${name}, Surname: ${surname}, Age: ${age}`);
+// }
+// example("Jaloliddin","Karshiev", 29)
+
+// const getAvr =function (){
+//     console.log("Jaloliddin, Welcome to WebBrain Academy");
+// }
+// getAvr()
+
+
+//ARROW FUNCTION
+// const getAvr = () =>{
+//     return "Jaloliddin, Welcome to WebBrain Academy"
+// }
+// console.log(getAvr());
+
+// const example = (id, password) =>{
+//     console.log(`id:${id}, password: ${password}`);
+// }
+// example("jaloliddin94", "J@1234567")
+
+
+// const telegram = (name, surname) =>{
+//     if(surname) console.log(name, surname);
+//     else console.log((name));
+// }
+
+// telegram("WebBrain","Academy" );
