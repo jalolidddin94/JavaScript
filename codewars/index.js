89512d9a957624e9f4632bfa2408c2e22bc36622
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-12-19 23:51:33 +0900

    codewars

---
+++ codewars/index.js
@@ -0,0 +1,22 @@
+// const example = (a, b) => {
+//     return 5**4
+// }
+// console.log(example());
+
+// const example1 = ( a) => {
+//    return a **0.5
+// }
+// console.log(example1(81));
+
+// function greet(Jaloliddin){
+//     return `Hello, " ${Jaloliddin} " how are you doing today?`
+//   }
+//   console.log(greet("Jaloliddin"));
+
+
+
+
+
+
+
+
