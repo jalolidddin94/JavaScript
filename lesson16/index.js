cbb0a475a3fa6b98832fc6cb54147b27de2d5435
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-12-21 23:54:47 +0900

    recursive function, new set

---
+++ lesson16/index.js
@@ -0,0 +1,85 @@
+// new Date();
+// let d = new Date () ;
+// console.log(d);
+// console.log(d.getDate());
+// console.log(d.getDay());
+// console.log(d.getMonth());
+// console.log(d.getFullYear());
+// console.log(d.getHours());
+// console.log(d.getMinutes());
+// console.log(d.getSeconds());
+// console.log(d.getMilliseconds());
+
+////////////////////////////////////////////
+
+// recursion(recursive function);
+// const myRecurse = (num) => {
+//     console.log(num);
+
+//     let number = num - 2 ;
+
+//     if(number >= 4){
+        
+//         myRecurse(number) ;
+//     }
+
+
+// }
+// myRecurse(15)
+
+// const myRecurse = (raqam) =>{
+//     console.log(raqam);
+
+//     let raqam1 = raqam - 3 ;
+
+//     if(raqam1 >= 2){
+//         myRecurse(raqam1)
+//     }
+// };
+
+// myRecurse(10);
+
+////////////////////////////////////////
+
+// new Set();
+
+// const set = new Set([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7]);
+// console.log(set);
+
+// const set1 = [...set];
+// console.log(set1);
+
+// const set = new Set([4, 3, 2, 1]);
+
+// set.add({dustlar :"Assalomu alaykum"})
+
+// set.add("SET") Map'lumot qushish !
+// set.add('kup');
+// set.add("sonli");
+// set.add("Raqamlarni");
+// set.add("sort");
+// set.add("qilib");
+// set.add("beradi");
+
+// set.delete("kup") ma'lumotni uchirish !
+
+// set.clear() hammasini tozalash !
+
+// console.log(set.has("sort")); Ma'lumot bor yuqligini aniqlash !
+
+// console.log(set);
+
+// const set = new Set([9,8,7,6,5])
+// console.log(set.size); Necta ma'lumot borligini aniqlashda "SIZE" dan foydalanmiz !
+
+
+
+
+
+
+
+
+
+
+
+
