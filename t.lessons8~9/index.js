20d1eae6f282ee867db220035f4cd1c344db1a1e
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-12-25 23:43:23 +0900

    Math built in mathods, Number mathods and String

---
+++ t.lessons8~9/index.js
@@ -0,0 +1,67 @@
+// Math built in mathods
+
+// console.log(Math.pow(4,3));
+// console.log(Math.sqrt(81));
+// console.log(Math.cbrt(125));
+// console.log(Math.min(1,2,3,4,5,6,7));
+// console.log(Math.max(1,2,3,4,5,6,7));
+// console.log(Math.abs(-100));
+
+/////////////////////////////////////////////////////////
+
+//Number mathods
+
+// console.log(parseInt(Math.random() -899));
+// console.log(Math.round(10.5));
+// console.log(Math.floor(10.8));
+// console.log(Math.ceil(10.1234));
+// console.log(Number.isInteger(20));
+// console.log(typeof Number.parseInt(30.5));
+// console.log(typeof +25);
+// console.log(typeof +50.5.toFixed());
+
+/////////////////////////////////////////////////////////
+// String
+
+// const str = "Jaloliddin"
+// const str1 = "Karshiev"
+
+// console.log(str.substring(0 , 5));
+// console.log(str.slice(0,5));
+
+// console.log(str.substr(0,5));
+
+// console.log(str.toLocaleLowerCase());
+// console.log(str.toUpperCase());
+
+// console.log(str.trim());
+
+// console.log(str.startsWith("J"));
+// console.log(str.endsWith("n"));
+
+// console.log(str.padStart(14, "Boss"));
+// console.log(str.padEnd(14, "Boss"));
+
+// console.log(str.replace("Jalolid", "Xusnid"));
+
+// console.log(str.repeat(2));
+
+// console.log(str.concat(str1));
+
+// console.log(str.includes("din"));
+
+// console.log(str.indexOf("J"));
+// console.log(str.lastIndexOf("n"));
+
+// console.log(str.charAt(5));
+
+
+
+
+
+
+
+
+
+
+
