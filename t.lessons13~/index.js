2bec616552124822141409bf964cbd2c9157ee49
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2023-12-28 23:39:43 +0900

    new Map, Set

---
+++ t.lessons13~/index.js
@@ -0,0 +1,27 @@
+// let map = new Map();
+// map.name = "jaloliddin"
+// map.set (1234, "IT webbrain");
+// console.log(map.get(1234));
+// console.log(map.has(1234));
+// console.log(map.delete(1234));
+// console.log(map.clear());
+
+// let map = new Map([["name", "jaloliddin"],
+// ["title", "IT Center"]]);
+// // console.log(map);
+// // console.log(map.keys());
+// // console.log(map.values());
+// // console.log(map.entries());
+// // for(let values of map.values()){
+// //     console.log(values);
+// // };
+
+// console.log(Object.fromEntries(map));
+/////////////////////////////////////////////////////
+
+//Set
+
+// let obj = {name: "jaloliddin"};
+// let set = new Set();
+// set.add(obj);
+// console.log(obj);
