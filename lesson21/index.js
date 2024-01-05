69d045bbd40c91d3c4f88c89a09d0731ed512bc5
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2024-01-05 23:03:36 +0900

    fetch API

---
+++ lesson21/index.js
@@ -0,0 +1,13 @@
+// fetch API =>
+
+// fetch("https://jsonplaceholder.typicode.com/pos")
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error("404 saxifa topilmadi!")
+//         }
+//         return response.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((err) => console.log(err))
