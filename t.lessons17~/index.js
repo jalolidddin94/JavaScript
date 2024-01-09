c2bba5e6fe73aa0108e48ac56d4d0e561c1d83ee
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2024-01-09 23:48:04 +0900

    Flat

---
+++ t.lessons17~/index.js
@@ -0,0 +1,13 @@
+//Flat
+// let num = [1, 2, 3, [4, 5, 6, [7, 8, 9, 10]]];
+// console.log(num.flat(Infinity).reduce((s,c) => s+c));
+///////////////////////////////////////////////////////////
+
+// new Map
+// console.log(new Map([
+//     ["a", 1],
+//     ["b", 2],
+// ]).keys()
+// );
+
+// console.log(Array.from("jaloliddin",(v) =>v.toUpperCase()));
