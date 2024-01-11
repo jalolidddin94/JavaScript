6c86a0cb33a90eb13aea86b97ddc2006c11df47d
jaloliddin <jaloliddinkarshiev6767@gmail.com>
2024-01-11 23:48:02 +0900

    Function

---
+++ t.lessons18~/index.js
@@ -0,0 +1,30 @@
+// / FUNCTION EXPRESSION
+// const example = function(name,surname,age){
+//     console.log(`Name: ${name}, Surname: ${surname}, Age: ${age}`);
+// }
+// example("Jaloliddin","Karshiev", 29)
+
+// const getAvr =function (){
+//     console.log("Jaloliddin, Welcome to WebBrain Academy");
+// }
+// getAvr()
+
+
+//ARROW FUNCTION
+// const getAvr = () =>{
+//     return "Jaloliddin, Welcome to WebBrain Academy"
+// }
+// console.log(getAvr());
+
+// const example = (id, password) =>{
+//     console.log(`id:${id}, password: ${password}`);
+// }
+// example("jaloliddin94", "J@1234567")
+
+
+// const telegram = (name, surname) =>{
+//     if(surname) console.log(name, surname);
+//     else console.log((name));
+// }
+
+// telegram("WebBrain","Academy" );
